4cdfa09c4b50e84a17c85f3cc1d8ea438bd530ce
Joey <g00361752@gmit.ie>
2021-01-07 18:21:13 +0000

    added navigation between home screen and geoMaps/Galleryscreens

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import MapView from 'react-native-maps';
 
 function LoginScreen ({navigation}){
 
@@ -42,13 +43,35 @@ function LoginScreen ({navigation}){
 
 }
 
-function HomeScreen(){
+function HomeScreen({navigation}){
 
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
              <Text>Home Screen</Text>
 
-           </View>
+            <View style={styles.btnContainer2}>
+                          <TouchableOpacity
+                                style={styles.userBtn}
+                                onPress={() => navigation.navigate('GeoMap')}
+                            >
+                                <Text style={styles.btnTxt}>Geo Maps</Text>
+                                </TouchableOpacity>
+
+                                <TouchableOpacity
+                                     style={styles.userBtn}
+                                     onPress={() => navigation.navigate('Gallery')}
+                                >
+                                <Text style={styles.btnTxt}>Gallery</Text>
+                          </TouchableOpacity>
+            </View>
+
+            <TextInput
+                      style={styles.input}
+                      placeholder="Enter Conditions"
+                      />
+
+        </View>
+
       );
 
 }
@@ -92,6 +115,32 @@ function SignupScreen({navigation}){
 }
 
 
+function GeoMapScreen(){
+
+      return (
+        <View style={styles.container}>
+              <MapView style={styles.map} />
+            </View>
+
+      );
+
+}
+
+function GalleryScreen(){
+
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+             <Text>Gallery</Text>
+
+
+
+        </View>
+
+      );
+
+}
+
+
 const Stack = createStackNavigator();
 
 function App() {
@@ -101,6 +150,8 @@ function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="GeoMap" component={GeoMapScreen} />
+        <Stack.Screen name="Gallery" component={GalleryScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -145,11 +196,11 @@ input: {
      marginBottom: 10
 },
 
-/*btnContainer: {
-    //flexDirection: "row",
+btnContainer2: {
+    flexDirection: "row",
     justifyContent: "space-between",
     width: "90%"
-},*/
+},
 
 userBtn: {
 backgroundColor: "#00FF00",
@@ -162,6 +213,11 @@ backgroundColor: "#0000FF",
 padding: 15,
 width: "45%",
 },
+
+map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 
 btnTxt: {
 fontSize: 18,
